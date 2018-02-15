0afc794cc6f2a482cf9c3a7e41dd06023f2b51ff
Serhiy Shtokal <Telefonistttt@gmail.com>
2018-02-15 13:15:03 +0200

    initCommt

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.UP_menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.DOWN_menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.LEFT_menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.RIGHT_menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UP_menu,
+            this.DOWN_menu,
+            this.LEFT_menu,
+            this.RIGHT_menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // UP_menu
+            // 
+            this.UP_menu.Name = "UP_menu";
+            this.UP_menu.Size = new System.Drawing.Size(34, 20);
+            this.UP_menu.Text = "UP";
+            this.UP_menu.Click += new System.EventHandler(this.UP_menu_Click);
+            // 
+            // DOWN_menu
+            // 
+            this.DOWN_menu.Name = "DOWN_menu";
+            this.DOWN_menu.Size = new System.Drawing.Size(56, 20);
+            this.DOWN_menu.Text = "DOWN";
+            this.DOWN_menu.Click += new System.EventHandler(this.DOWN_menu_Click);
+            // 
+            // LEFT_menu
+            // 
+            this.LEFT_menu.Name = "LEFT_menu";
+            this.LEFT_menu.Size = new System.Drawing.Size(44, 20);
+            this.LEFT_menu.Text = "LEFT";
+            this.LEFT_menu.Click += new System.EventHandler(this.LEFT_menu_Click);
+            // 
+            // RIGHT_menu
+            // 
+            this.RIGHT_menu.Name = "RIGHT_menu";
+            this.RIGHT_menu.Size = new System.Drawing.Size(53, 20);
+            this.RIGHT_menu.Text = "RIGHT";
+            this.RIGHT_menu.Click += new System.EventHandler(this.RIGHT_menu_Click);
+            // 
+            // upButton
+            // 
+            this.upButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.upButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.upButton.Location = new System.Drawing.Point(3, 3);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(209, 54);
+            this.upButton.TabIndex = 1;
+            this.upButton.Text = "UP";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.downButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.downButton.Location = new System.Drawing.Point(218, 3);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(209, 54);
+            this.downButton.TabIndex = 2;
+            this.downButton.Text = "DOWN";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.leftButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.leftButton.Location = new System.Drawing.Point(433, 3);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(209, 54);
+            this.leftButton.TabIndex = 3;
+            this.leftButton.Text = "LEFT";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rightButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rightButton.Location = new System.Drawing.Point(648, 3);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(209, 54);
+            this.rightButton.TabIndex = 4;
+            this.rightButton.Text = "RIGHT";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.upButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rightButton, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.downButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.leftButton, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 240);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(860, 60);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // moveButton
+            // 
+            this.moveButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.moveButton.Location = new System.Drawing.Point(400, 120);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(75, 23);
+            this.moveButton.TabIndex = 6;
+            this.moveButton.Text = "Move me!";
+            this.moveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 312);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem UP_menu;
+        private System.Windows.Forms.ToolStripMenuItem DOWN_menu;
+        private System.Windows.Forms.ToolStripMenuItem LEFT_menu;
+        private System.Windows.Forms.ToolStripMenuItem RIGHT_menu;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button moveButton;
     }
 }
 
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -12,9 +12,84 @@ namespace lab1
 {
     public partial class Form1 : Form
     {
+        private float diagonal=200;
+        private int speed = 20;
         public Form1()
         {
             InitializeComponent();
+            this.moveButton.Width = (int)(4 * diagonal / Math.Sqrt(17));
+            this.moveButton.Height = (int)(diagonal / Math.Sqrt(17));
+            int X = Size.Width / 2-moveButton.Width/2;
+            int Y = Size.Height / 2-moveButton.Height/2-tableLayoutPanel1.Height;
+            this.moveButton.Location = new Point(X,Y);
+            
+        }
+
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            int X = moveButton.Location.X;
+            int Y = moveButton.Location.Y - speed;
+            if (Y >= menuStrip1.Size.Height)
+                this.moveButton.Location = new Point(X, Y);
+            else
+                MessageBox.Show("Form border!");
+        }
+
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            int X = moveButton.Location.X;
+            int Y = moveButton.Location.Y + speed;
+            if (Y <= this.Size.Height- tableLayoutPanel1.Size.Height-moveButton.Height-40)
+                this.moveButton.Location = new Point(X, Y);
+            else
+                MessageBox.Show("Form border!");
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            int X = moveButton.Location.X- speed;
+            int Y = moveButton.Location.Y;
+            if (X >= 0)
+                this.moveButton.Location = new Point(X, Y);
+            else
+                MessageBox.Show("Form border!");
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            int X = moveButton.Location.X+ speed;
+            int Y = moveButton.Location.Y;
+            if (X <= this.Size.Width-moveButton.Size.Width-14)
+                this.moveButton.Location = new Point(X, Y);
+            else
+                MessageBox.Show("Form border!");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int X = Size.Width / 2 - moveButton.Width / 2;
+            int Y = Size.Height / 2 - moveButton.Height / 2 - tableLayoutPanel1.Height;
+            this.moveButton.Location = new Point(X, Y);
+        }
+
+        private void UP_menu_Click(object sender, EventArgs e)
+        {
+            upButton_Click(sender, e);
+        }
+
+        private void DOWN_menu_Click(object sender, EventArgs e)
+        {
+            downButton_Click(sender, e);
+        }
+
+        private void LEFT_menu_Click(object sender, EventArgs e)
+        {
+            leftButton_Click(sender, e);
+        }
+
+        private void RIGHT_menu_Click(object sender, EventArgs e)
+        {
+            rightButton_Click(sender, e);
         }
     }
 }
